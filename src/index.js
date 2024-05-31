a00a38c1f43e813e06cc7b18a66cc253509ef5c8
vasenkom <m.vasenko@yandex.ru>
2024-05-31 15:40:58 +0200

    add loadHome() which combines all web parts: hero, menu, contact, footer; correct the logic of homeBtn and menuBtn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import _ from 'lodash';
 import './style.css';
 
-import {contactMapAdd} from './contact.js'
-import {menuAdd} from './menu.js'
-import {componentAdd} from './home.js'
+import { contactMapAdd } from './contact.js'
+import { menuAdd } from './menu.js'
+
+import { heroAdd } from './hero.js'
+import { footerAdd } from './footer.js';
+
+const contentBox = document.querySelector('#content');
 
 const nav = document.querySelector(".nav");
 const header = document.querySelector(".header");
@@ -12,12 +16,51 @@ const homeBtn = document.querySelector(".homeBtn");
 const menuBtn = document.querySelector(".menuBtn");
 const contactBtn = document.querySelector(".contactBtn");
 
-homeBtn.addEventListener('click', function() {
-    const { mainBlock, footer } = componentAdd();
+function loadHome() {
+    // Clear previous content
+    contentBox.innerHTML = '';
+
+    //add hero section
+    const {mainBlock} = heroAdd();
     contentBox.appendChild(mainBlock);
+
+    //add menu section
+    const {menuMainBlock} = menuAdd();
+    contentBox.appendChild(menuMainBlock);
+
+    //add contact section
+    const {contactMainBlock} = contactMapAdd();
+    contentBox.appendChild(contactMainBlock);
+
+    //add footer section
+    const {footer} = footerAdd();
+    contentBox.appendChild(footer);
+}
+
+// Load home content initially
+loadHome();
+
+homeBtn.addEventListener('click', loadHome);
+
+menuBtn.addEventListener('click', function() {
+    contentBox.innerHTML = '';
+    // Load menu content
+    const {menuMainBlock} = menuAdd();
+    contentBox.appendChild(menuMainBlock);
+
+    //add footer section
+    const {footer} = footerAdd();
     contentBox.appendChild(footer);
 })
 
-menuBtn.addEventListener('click', function() {
-    contactMapAdd();
+contactBtn.addEventListener('click', function() {
+    contentBox.innerHTML = '';
+
+    // Load contact content
+    const {contactMainBlock} = contactMapAdd();
+    contentBox.appendChild(contactMainBlock);
+
+    //add footer section
+    const {footer} = footerAdd();
+    contentBox.appendChild(footer);
 })
